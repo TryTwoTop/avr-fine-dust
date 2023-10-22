480cb7d3398da66588c4a2307969716778f61392
TryTwoTop <trytwotop@gmail.com>
2023-10-22 20:03:31 +0900

    feat: Pin 기능을 설정하는 함수 작성하기 [AVR-FINE-DUST12]

--- domain/pin/service/pin.c
+++ domain/pin/service/pin.c
@@ -10,3 +10,13 @@ void pin_set (pin_t *pin, gpio_port_t gpio_port, uint8_t gpio_pin)
     pin->gpio_port = gpio_port;
     pin->gpio_pin = gpio_pin;
 }
+
+void pin_set_mode (pin_t *pin, uint8_t pin_mode)
+{
+    gpio_set_pin_mode(pin->gpio_port, pin->gpio_pin, pin_mode);
+}
+
+void pin_write (pin_t *pin, uint8_t pin_level)
+{
+    gpio_write_pin(pin->gpio_port, pin->gpio_pin, pin_level);
+}
--- domain/pin/service/pin.h
+++ domain/pin/service/pin.h
@@ -3,6 +3,14 @@
 
 #include "../../gpio/service/gpio.h"
 
+/* PIN MODE */
+#define INPUT   GPIO_PIN_MODE_INUPT
+#define OUTPUT  GPIO_PIN_MODE_OUTPUT
+
+/* PIN LEVEL */
+#define HIGH    GPIO_PIN_SET
+#define LOW     GPIO_PIN_RESET
+
 /* Value Object */
 typedef struct
 {
@@ -10,7 +18,19 @@ typedef struct
     uint8_t gpio_pin;
 } pin_t;
 
+/*
+    Example
+
+    pin_t led;
+    pin_init();
+    pin_set(&led, GPIOD, GPIO_PIN_2);
+    pin_set_mode(&led, OUTPUT);
+    pin_write(&led, HIGH);
+*/
+
 void pin_init (void);
 void pin_set (pin_t *, gpio_port_t, uint8_t);
+void pin_set_mode (pin_t *, uint8_t);
+void pin_write (pin_t *, uint8_t);
 
 #endif //__PIN_SERVICE_PIN_H__
